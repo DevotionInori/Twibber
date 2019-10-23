622fc909cb7931fc5f255edfc1a0ccdee66a65bd
B17041530宋亚东 <843768290@qq.com>
2019-10-23 15:03:01 +0800

    0.0.0001

---
+++ app/src/main/java/com/SydShp/twibber/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.SydShp.twibber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_DISPLAY_LENGHT= 3000;    //延迟2秒
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);	//第二个参数即为执行完跳转后的Activity
+                startActivity(intent);
+                SplashActivity.this.finish();   //关闭SplashActivity，将其回收，否则按返回键会返回此界面
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+}
