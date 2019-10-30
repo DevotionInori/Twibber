2b85aa0a8a2197fa39be602dcd1225e646a400e4
songyadong <843768290@qq.com>
2019-10-30 16:18:39 +0800

    登陆界面改viewpage

--- app/src/main/java/com/SydShp/twibber/LogIn.java
+++ app/src/main/java/com/SydShp/twibber/LogIn.java
@@ -2,7 +2,6 @@ package com.SydShp.twibber;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
@@ -14,22 +13,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.SydShp.twibber.model.user;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.litepal.LitePal;
 
 import java.util.ArrayList;
 
 public class LogIn extends AppCompatActivity {
 
     private Button btnLogIn;
-    private Button btnLogOn;
-    private Button btnReg;
+    private Button btnRegister;
     private ViewPager vp;
     private ViewPagerTitle hsv;
-
+    private TextInputEditText idLogIn;
+    private TextInputEditText pwdLogIn;
+    private TextInputEditText idRegister;
+    private TextInputEditText pwdRegister;
+    private TextInputEditText nameRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        LitePal.initialize(this);
 
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
@@ -77,8 +88,32 @@ public class LogIn extends AppCompatActivity {
     public void initPage(View view, int position) {
         if (position % 2 == 0) {
             btnLogIn=view.findViewById(R.id.btn_log_in);
+            idLogIn=view.findViewById(R.id.log_in_uesrid);
+            pwdLogIn=view.findViewById(R.id.log_in_password);
+            btnLogIn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
         } else {
-            btnLogOn=view.findViewById(R.id.btn_register);
+            btnRegister =view.findViewById(R.id.btn_register);
+            idRegister=view.findViewById(R.id.register_uesrid);
+            pwdRegister=view.findViewById(R.id.register_password);
+            nameRegister=view.findViewById(R.id.register_name);
+            btnRegister.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //user user=new user();
+                    //user.setLoginId(idRegister.getText().toString());
+                    //user.setPassWd(pwdRegister.getText().toString());
+                    //user.setName(nameRegister.getText().toString());
+                    //user.save();
+                    Snackbar.make(v,"注册成功!",Snackbar.LENGTH_LONG).show();
+                    pwdRegister.setText("");
+                    nameRegister.setText("");
+                }
+            });
         }
     }
 
